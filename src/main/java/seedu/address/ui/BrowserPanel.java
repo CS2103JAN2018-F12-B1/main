2cc9a64fab42fde6d7fa5f9470480d97858c763e
DESKTOP-9K2NI32\Liu Yiming <e0031565@u.nus.edu>
2018-03-15 03:59:38 +0800

    Updates BrowserPanel to replace default page with nus logo

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -20,7 +20,7 @@ import seedu.address.model.person.Person;
  */
 public class BrowserPanel extends UiPart<Region> {
 
-    public static final String DEFAULT_PAGE = "default.html";
+    public static final String DEFAULT_PAGE = "/docs/NUS_logo.html";
     public static final String SEARCH_PAGE_URL =
             "https://se-edu.github.io/addressbook-level4/DummySearchPage.html?name=";
 
@@ -53,7 +53,7 @@ public class BrowserPanel extends UiPart<Region> {
      * Loads a default HTML file with a background that matches the general theme.
      */
     private void loadDefaultPage() {
-        URL defaultPage = MainApp.class.getResource(FXML_FILE_FOLDER + DEFAULT_PAGE);
+        URL defaultPage = MainApp.class.getResource(DEFAULT_PAGE);
         loadPage(defaultPage.toExternalForm());
     }
 
